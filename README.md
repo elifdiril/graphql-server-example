bc1b6ab324eff157df80b696aec0d02e0502ce07
elifdiril <elifdiril6@gmail.com>
2022-01-01 10:55:04 +0300

    custom types added, readme file arranged

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # graphql-server-example
 ## for playing apollo
+
+## In order to run this project in your local environment
+
+ <br/>
+
+```bash
+git clone https://github.com/elifdiril/graphql-server-example.git
+```
+
+And then
+
+```bash
+npm install
+```
+
+to install all the dependencies.
+
+Finally,
+
+```bash
+npm run dev
+```
+
+to start the development mode.
--- index.js
+++ index.js
@@ -1,30 +1,59 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
 
+const author = {
+    id: "1",
+    name: "Reşat Nuri",
+    surname: "Güntekin",
+    books: [{
+        id: "erwsedf332",
+        title: "Test",
+        score: 7,
+        isPublished: false
+    },
+    {
+        id: "erwsedf33sd",
+        title: "Test 2",
+        score: 8,
+        isPublished: true
+    }]
+}
+
+const book = {
+    id: "sdfsdfsdfc23",
+    title: 'Çalıkuşu',
+    author,
+    score: 9.8,
+    isPublished: true
+};
+
 const typeDefs = gql`
+
+  type Author {
+    id: ID!
+    name: String
+    surname: String
+    books: [Book!]
+  }
   
   type Book {
     id: ID!
     title: String
-    author: String
+    author: Author
     isPublished: Boolean
     score: Float
   }
 
   type Query {
-    books: [Book!]
+    book: Book
+    author: Author
   }
 `;
 
 const resolvers = {
     Query: {
-        books: () => [{
-            id: "sdfsdfsdfc23",
-            title: 'Çalıkuşu',
-            author: 'Reşat Nuri Güntekin',
-            score: 9.8,
-            isPublished: true
-        }],
+        book: () => book,
+        author: () => author
     }
 };
 
